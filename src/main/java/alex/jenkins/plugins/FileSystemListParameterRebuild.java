078059f952c08211d8cace89db52bdad98641248
muse <muse@muse-mbp.local>
2023-04-27 15:19:00 +0900

    Adjust to rebuild plugin new interface

--- src/main/java/alex/jenkins/plugins/FileSystemListParameterRebuild.java
+++ src/main/java/alex/jenkins/plugins/FileSystemListParameterRebuild.java
@@ -3,15 +3,22 @@ package alex.jenkins.plugins;
 import com.sonyericsson.rebuild.RebuildParameterPage;
 import com.sonyericsson.rebuild.RebuildParameterProvider;
 import hudson.Extension;
+import hudson.model.ParameterDefinition;
 import hudson.model.ParameterValue;
 
 @Extension(optional = true)
 public class FileSystemListParameterRebuild extends RebuildParameterProvider {
 
+    /**
+     * Changed by Kenny
+     * @param definition
+     * @param parameterValue
+     * @return
+     */
     @Override
-    public RebuildParameterPage getRebuildPage(ParameterValue parameterValue) {
+    public RebuildParameterPage getRebuildPage(ParameterDefinition definition, ParameterValue parameterValue) {
         if (parameterValue instanceof FileSystemListParameterValue) {
-            return new RebuildParameterPage(parameterValue.getClass(),"value.jelly");
+            return new RebuildParameterPage(parameterValue.getClass(),"value.jelly", definition);
         }
         return null;
     }
